1521e2ccfec8c99574046cb08944811bbb97786c
AlirezaFatahi <afatahi201580@gmail.com>
2024-11-17 18:38:30 +0330

    new commit3

--- src/app/lib/placeholder-data.js
+++ src/app/lib/placeholder-data.js
@@ -16,5 +16,47 @@ export const products = [
         name: "ماوس",
         price: 300_000,
         qty: 20
+    },
+    {
+        id: 2,
+        category: "لوازم جانبی",
+        name: "صفحه کلید",
+        price: 400_000,
+        qty: 25
+    },
+    {
+        id: 3,
+        category: "لوازم جانبی",
+        name: "هارد اکسترنال",
+        price: 3_000_000,
+        qty: 10
+    },
+    {
+        id: 4,
+        category: "لب تاب",
+        name: "اپل",
+        price: 200_000_000,
+        qty: 2
+    },
+    {
+        id: 5,
+        category: "لب تاب",
+        name: "ایسوس",
+        price: 30_000_000,
+        qty: 5
+    },
+    {
+        id: 6,
+        category: "لوازم جانبی",
+        name: "اسپیکر",
+        price: 800_000,
+        qty: 15
+    },
+    {
+        id: 7,
+        category: "لوازم جانبی",
+        name: "فن لب تاب",
+        price: 700_000,
+        qty: 13
     }
 ];
